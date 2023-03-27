377195eff28304b2234d5e9b58d114fca5cd471a
mdmahafuzur47 <mdmahafuzur4747@gmail.com>
2023-03-27 14:50:35 +0600

    Nav Bar Completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './Header.css'
+import logo from "../../images/Logo.svg"
 
 const Header = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
+        <div className='header'>
+            <nav className='navigation-bar'>
+                <img src={logo} alt="" />
+                <div>
+                    <a href="/order">Order</a>
+                    <a href="/review">Order Review</a>
+                    <a href="/inventory">Manage Inventory</a>
+                    <a href="/login">Login</a>
+                </div>
+            </nav>
         </div>
     );
 };
